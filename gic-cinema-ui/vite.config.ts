df7ca2a759b32213a1877a4777bc5d1ee4e0cd1c
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2025-10-06 20:26:26 +0800

    Initial commit for react typescript (with vite)

---
+++ gic-cinema-ui/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/cinema': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
+      '/cinemalayout': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
+      '/bookings': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
+    }
+  }
+})
