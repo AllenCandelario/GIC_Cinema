2fb302412ea2366103aaa79980c485cfb1713fe2
Allen Candelario <chrisallen1394@gmail.com>
2025-10-07 14:10:19 +0800

    Default vite.config.ts proxy to http

--- gic-cinema-ui/vite.config.ts
+++ gic-cinema-ui/vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/cinema': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
-      '/cinemalayout': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
-      '/bookings': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
-      '/reset': { target: 'https://localhost:7085', changeOrigin: true, secure: false },
+      '/cinema': { target: 'http://localhost:5135', changeOrigin: true, secure: false },
+      '/cinemalayout': { target: 'http://localhost:5135', changeOrigin: true, secure: false },
+      '/bookings': { target: 'http://localhost:5135', changeOrigin: true, secure: false },
+      '/reset': { target: 'http://localhost:5135', changeOrigin: true, secure: false },
     }
   }
 })
