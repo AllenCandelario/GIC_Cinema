520a7d36f228ac5356f8e700d4713264d0b86a2c
DESKTOP-R07R65R\chris <chrisallen1394@gmail.com>
2025-10-23 14:06:32 +0800

    new test class BookingServiceTests.cs for logic + state tests

---
+++ GIC_Cinema.Tests/BookingServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using GIC_Cinema.Core.Application.Services;
+using GIC_Cinema.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIC_Cinema.Tests
+{
+    public class BookingServiceTests
+    {
+        [Fact]
+        public void CanBook_True_WhenWithinCapacity()
+        {
+            var m = new CinemaMovie("Inception", rows: 2, seatsPerRow: 3); // 2 x 3 = 6 seats
+            Assert.True(BookingService.CanBook(m, 6));
+            Assert.True(BookingService.CanBook(m, 1));
+        }
+
+        [Fact]
+        public void CanBook_False_WhenExceedsCapacity()
+        {
+            var m = new CinemaMovie("Inception", rows: 2, seatsPerRow: 3); // 2 x 3 = 6 seats
+            Assert.False(BookingService.CanBook(m, 7));
+            Assert.False(BookingService.CanBook(m, 0));
+            Assert.False(BookingService.CanBook(m, -1));
+        }
+    }
+}
